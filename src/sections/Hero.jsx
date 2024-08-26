c1de9e4470c418856ed06fbb465db5de92a478c0
victoriaEssien <victoriaessien857@gmail.com>
2024-08-26 16:23:48 +0100

    altered some hero text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,10 +9,10 @@ function Hero() {
             {/* Hero text */}
             <div className=''>
                 <h1 className='text-4xl md:text-[54px] font-os font-bold text-gray-600 mt-4 md:mt-14 w-full md:w-[590px] leading-normal md:leading-snug'>Discover, Rate, and Connect!</h1>
-                <p className='mt-6 md:mt-3 font-os font-normal w-full md:w-[516px] leading-relaxed text-lg text-gray-500'>Join the ultimate K-drama community. Share your thoughts, review your favorite series, and connect with fans around the globe.</p>
+                <p className='mt-6 md:mt-3 font-os font-normal w-full md:w-[516px] leading-relaxed text-lg text-gray-500'>Join our growing community! Share your thoughts, manage your kdrama watchlist, review your favorite series, and connect with fans around the globe.</p>
                 <div className='mt-8 md:mt-6'>
                 <Link to="">
-                  <button type='button' className='bg-[#2E7D32] rounded-md text-center block md:inline font-lato text-white text-lg font-normal w-full md:w-fit px-7 py-3.5 hover:bg-primary-800'>Join the community!</button>
+                  <button type='button' className='bg-[#2E7D32] rounded-md text-center block md:inline font-lato text-white text-lg font-normal w-full md:w-fit px-7 py-3.5 hover:bg-primary-800'>Get started!</button>
                 </Link>
                 {/* <Link to='/login'>
                   <button type='button' className='border border-primary-600 rounded-[10px] text-center block md:inline font-lato text-primary-600 text-[18px] font-normal w-full md:w-[190px] px-[30px] py-[14px] mt-4'>Review your CV</button>
